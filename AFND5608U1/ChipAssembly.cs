88a0925594ade2e495129daca1cdae1b2f17c2cb
JuliProg <taraskit@gmail.com>
2021-03-18 22:09:50 +0200

    update chip to datasheet

--- AFND5608U1/ChipAssembly.cs
+++ AFND5608U1/ChipAssembly.cs
@@ -32,17 +32,17 @@ namespace AFND5608U1
 
         ChipAssembly()
         {
-            myChip.devManuf = "SAMSUNG";
+            myChip.devManuf = "ATOSolution";
             myChip.name = "AFND5608U1";
-            myChip.chipID = "ECF1001540";      // device ID - ECh F1h 00h 15h 40h (k9f1g08u0d_00.pdf page 36)
+            myChip.chipID = "ffffffff";      // device ID - not found
 
             myChip.width = Organization.x8;    // chip width - 8 bit
-            myChip.bytesPP = 2048;             // page size - 2048 byte (2Kb)
-            myChip.spareBytesPP = 64;          // size Spare Area - 64 byte
-            myChip.pagesPB = 64;               // the number of pages per block - 64 
-            myChip.bloksPLUN = 1024;           // number of blocks in CE - 1024
+            myChip.bytesPP = 512;             // page size - 512 byte 
+            myChip.spareBytesPP = 16;          // size Spare Area - 16 byte
+            myChip.pagesPB = 32;               // the number of pages per block - 32 
+            myChip.bloksPLUN = 2048;           // number of blocks in CE - 2048
             myChip.LUNs = 1;                   // the amount of CE in the chip
-            myChip.colAdrCycles = 2;           // cycles for column addressing
+            myChip.colAdrCycles = 1;           // cycles for column addressing
             myChip.rowAdrCycles = 2;           // cycles for row addressing 
             myChip.vcc = Vcc.v3_3;             // supply voltage
 
@@ -86,7 +86,7 @@ namespace AFND5608U1
 
             myChip.registers.Add(                  // https://github.com/JuliProg/Wiki/wiki/ID-Register
                 "Id Register").
-                Size(5).
+                Size(4).
                 Operations("ReadId_90h").               
                 Interpretation(ID_interpreting);
 
